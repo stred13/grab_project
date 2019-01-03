23b5ff47f681c47d041c7b9ec52eaf27fc50fa6f
1742009 <trankhacdoanh95@gmail.com>
2019-01-03 17:11:04 +0700

    tính khoảng cách hebersin

--- driver/src/App.vue
+++ driver/src/App.vue
@@ -30,6 +30,31 @@ export default {
     };
   },
   methods: {
+     herbesin(evt) {
+      // console.log(this.myLatlng.lat);
+      var R = 6371e3; // metres
+      var φ1 = (this.myLatlng.lat * Math.PI) / 180;
+      var φ2 = (evt.latLng.lat() * Math.PI) / 180;
+      var Δφ = ((evt.latLng.lat() - this.myLatlng.lat) * Math.PI) / 180;
+      var Δλ = ((evt.latLng.lng() - this.myLatlng.lng) * Math.PI) / 180;
+
+      var a =
+        Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+      var d = R * c;
+      console.log("Khoảng cách :" + d);
+      //console.log("Khoảng cách 1:" + evt.latLng);
+      if (d < 100) {
+        this.myLatlng.lat = evt.latLng.lat();
+        this.myLatlng.lng = evt.latLng.lng();
+        this.markers[0].position.lat = evt.latLng.lat();
+        this.markers[0].position.lng = evt.latLng.lng();
+        //console.log(this.markers[0].position.lat
+      }
+       //console.log("Khoảng cách 2:" + this.markers[0].lat);
+    }
   }
 };
 </script>
--- driver/src/main.js
+++ driver/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as VueGoogleMaps from "vue2-google-maps";
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyAPgoNwcVbT9ALPvVs1Ze5SAFwyVr1FT_g",
+    libraries: "places"
+  }
+});
 Vue.config.productionTip = false
 
 new Vue({
