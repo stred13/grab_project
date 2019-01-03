523590ddae17698bb045c4eb00a3c928b30eca49
stred13 <trietdang96@gmail.com>
2019-01-03 13:30:30 +0700

    back end node js

---
+++ back-end/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`api running on port ${port}`);
+})
