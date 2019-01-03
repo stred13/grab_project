dc160999a1bd9dd838409b9a2599ed709fec2aa2
stred13 <trietdang96@gmail.com>
2019-01-03 13:49:44 +0700

    create folder routes,controller,repo
    receiver.controller.js (login,token,verying)
    receiver.repo.js
    receiver.route.js

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const connection = require('./models/connection');
+
+connection.connectmg;
+
+
 var port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`api running on port ${port}`);
---
+++ back-end/controller/receiver.controller.js
@@ -0,0 +1,64 @@
+var jwt = require('jsonwebtoken');
+var express = require('express');
+var SHA256 = require('crypto-js/sha256');
+
+//
+var app = express();
+var eventEmitter = require('eventemitter3');
+var emitter = new eventEmitter();
+
+const config = require('../config.js');
+var receiRepo = require('../repo/receiver.repo');
+
+module.exports.getListReceivers = async function (req, res) {
+    try {
+        //console.log(req);
+        var rec = await Receiver.find();
+        res.statusCode = 200;
+        res.send(JSON.stringify(rec));
+    } catch (error) {
+        res.statusCode = 500;
+        console.log(error);
+    }
+}
+
+module.exports.getRe_by_userName = async (req, res) => {
+    try {
+        var data = req.body;
+        console.log(data);
+        var rec = await receiRepo.findoneReceiver(data);
+        //console.log(rec);
+        if (rec != null) {
+            var payload = {
+                id: rec._id,
+                user_name: rec.user_name
+            }
+            
+            var actoken = jwt.sign(payload, config.secret, { expiresIn: "10m" });
+            var rft = "";
+            //receiRepo.saveRFT({id:payload.id,rft:rft});
+
+            res.statusCode = 200;
+            res.json({
+                payload,
+                actoken: actoken
+                //rfToken: rft
+            });
+        } else {
+            res.json({ success: false })
+        }
+    } catch (error) {
+        res.statusCode = 500;
+        console.log(error);
+
+    }
+}
+
+module.exports.verifying_User = (req, res) => {
+    try {
+        return res.json(req.token_payload);
+    } catch (error) {
+        res.statusCode = 500;
+        console.log(error);
+    }
+}
