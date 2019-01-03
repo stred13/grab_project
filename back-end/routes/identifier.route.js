beffff9d78b92d67347fb58b46a025b72979a71c
stred13 <trietdang96@gmail.com>
2019-01-03 23:01:15 +0700

    update fulladd
    identifier.controller.js
    identifier.repo.js
    identifier.route.js

--- back-end/routes/identifier.route.js
+++ back-end/routes/identifier.route.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var receiver = require('../controllers/receivers.controller');
 var tokenMiddleware = require('../middlewares/token');
 var events = require('../controllers/event');
+var identifier = require('../controllers/identifier.controller');
+
 
 //router.get('/',receiver.getListReceivers);
 
-router.post('/identifier/login', receiver.getRe_by_userName);
-
+router.post('/login',receiver.getRe_by_userName);
+router.post('/update',identifier.updateFulladd);
 //router.get('/login',tokenMiddleware.verifyAccessToken,receiver.verifying_User);
 
 module.exports = router;
