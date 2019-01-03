20cf1c99a2e736bf19476d6a2f18e9be841beeb9
tien-dv2 <tien-dv2@fujinet.net>
2019-01-04 00:31:01 +0700

    folder identifier
    UI identifier

---
+++ identifier/src/views/Identifier.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid mt-5">
+    <div class="row">
+      <div class="col-4">
+        <GmapMap
+          v-bind:center="{lat:10.801988, lng:106.618493}"
+          v-bind:zoom="16"
+          map-type-id="terrain"
+          style="width: 500px; height: 300px"
+        >
+          <GmapMarker
+            v-bind:key="index"
+            v-for="(m, index) in markers"
+            v-bind:position="m.position"
+            v-bind:clickable="true"
+          />
+        </GmapMap>
+      </div>
+      <div class="col-8">
+        <listtable />
+      </div>
+    </div>
+  </div>
+</template>
+
+
---
+++ identifier/src/views/LogIn.vue
@@ -0,0 +1,75 @@
+<template>
+  <b-container>
+    <b-row align-h="center">
+      <b-col
+        cols="4"
+        class="text-center"
+      >
+        <h1>Recivers</h1>
+      </b-col>
+    </b-row>
+    <b-row
+      align-h="center"
+      class="mt-2"
+    >
+      <b-col cols="4">
+        <b-card class="p-2 text-left">
+          <b-form
+            @submit.prevent="onSubmit"
+            @reset="onReset"
+            v-if="show"
+          >
+            <b-form-group
+              id="exampleInputGroup1"
+              label="User name:"
+              label-for="exampleInput1"
+            >
+              <b-form-input
+                id="exampleInput1"
+                type="text"
+                v-model="form.user_name"
+                required
+                placeholder="Enter user name"
+              >
+              </b-form-input>
+            </b-form-group>
+            <b-form-group
+              id="exampleInputGroup2"
+              label="Password:"
+              label-for="exampleInput2"
+            >
+              <b-form-input
+                id="exampleInput2"
+                type="text"
+                v-model="form.pass"
+                required
+                placeholder="Enter password"
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="exampleGroup4">
+              <b-form-checkbox-group
+                v-model="form.checked"
+                id="exampleChecks"
+              >
+                <b-form-checkbox value="me">Check me out</b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group>
+            <div class="d-flex justify-content-between">
+              <b-button
+                type="submit"
+                variant="primary"
+              >Submit</b-button>
+              <b-button
+                type="reset"
+                variant="danger"
+                class="ml-1"
+              >Reset</b-button>
+            </div>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
+    <b-row></b-row>
+  </b-container>
+</template>
